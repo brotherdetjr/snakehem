5c30b36c6a7d5d45fa8dc6488e28ef1b893c0e01
brotherdetjr <brotherdetjr@gmail.com>
2026-02-15 01:11:31 +0000

    Reposition perf tracker info

--- game/unshaded/perftracker/draw.go
+++ game/unshaded/perftracker/draw.go
@@ -8,32 +8,34 @@ import (
 	"golang.org/x/image/colornames"
 )
 
+const (
+	textX   = 15
+	padding = 4
+)
+
 func (p *PerfTracker) Draw(screen *ebiten.Image) {
 	stats := p.GetStats()
 	if stats.SampleCount < 10 {
 		// Wait for enough samples before displaying
 		return
 	}
-
 	lines := stats.AsString()
-	y := 20
-
+	var lineSpacing = common.Adhoc8Height
+	y := screen.Bounds().Dy() - len(lines)*lineSpacing - 2*padding
 	// First line (TPS/FPS) always in light grey
-	adhoc8.Font.DrawString(screen, 15, y, lines[0], colornames.Lightgrey)
-	y += common.Adhoc8Height + 1
-
+	adhoc8.Font.DrawString(screen, textX, y, lines[0], colornames.Lightgrey)
+	y += lineSpacing
 	// Update line - red if warning, otherwise light grey
 	updateColor := colornames.Lightgrey
 	if stats.UpdateWarning {
 		updateColor = colornames.Red
 	}
-	adhoc8.Font.DrawString(screen, 15, y, lines[1], updateColor)
-	y += common.Adhoc8Height + 1
-
+	adhoc8.Font.DrawString(screen, textX, y, lines[1], updateColor)
+	y += lineSpacing
 	// Draw line - red if warning, otherwise light grey
 	drawColor := colornames.Lightgrey
 	if stats.DrawWarning {
 		drawColor = colornames.Red
 	}
-	adhoc8.Font.DrawString(screen, 15, y, lines[2], drawColor)
+	adhoc8.Font.DrawString(screen, textX, y, lines[2], drawColor)
 }
